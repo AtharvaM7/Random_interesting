83b5f527784aaec4c178e500a859bf4bf301af5e
AtharvaM7 <2023csb1104@iitrpr.ac.in>
2023-11-07 01:01:02 +0530

    Image_converter working

--- image_converter.py
+++ image_converter.py
@@ -32,4 +32,53 @@ print(image_array[row, col])
 # Change the pixel value to 0
 image_array[row, col] = 0
 
+''' 
+The i,j entry of the matrix is the pixel value at row i and column j which is
+deleted by the above code.
+Swap the ith row with the last row.
+Swap the jth column with the last column.
+Create a new matrix A with the last column deleted.
+A is a rectangular matrix.
+Express the last row of A as a linear combination of the other rows.
+Find the coefficients of the linear combination using least squares method.
+Multiply the last columns of the original matrix by the coefficients and print the
+result.
+'''
+# Uncomment the following code to see the result
+
+############################# ROW-WISE #########################################
+
+## Swap the ith row with the last row
+#image_array2[[row, image_array2.shape[0]-1]] = image_array2[[image_array2.shape[0]-1, row]]
+
+## Swap the jth column with the last column
+#image_array2[:, [col, image_array2.shape[1]-1]] = image_array2[:, [image_array2.shape[1]-1, col]]
+
+## Create a new matrix A with the last row deleted
+#A = image_array2[:-1, :-1]
+
+## Express the last column of A as a linear combination of the other columns
+## Find the coefficients of the linear combination using least squares method
+#coeff = np.linalg.lstsq(A, image_array2[:-1, -1], rcond=None)[0]
+
+## Multiply the last row of the original matrix by the coefficients and print the result
+#print(np.dot(coeff, image_array2[-1, :-1]))
+
+############################## COLUMN-WISE ######################################
+
+## Swap the ith row with the last row
+#image_array2[[row, image_array2.shape[0]-1]] = image_array2[[image_array2.shape[0]-1, row]]
+
+## Swap the jth column with the last column
+#image_array2[:, [col, image_array2.shape[1]-1]] = image_array2[:, [image_array2.shape[1]-1, col]]
+
+##Create a new matrix A with the last row deleted
+#A = image_array2[:-1, :-1]
+
+## Express the last column of A as a linear combination of the other columns
+## Find the coefficients of the linear combination using least squares method
+#coeff = np.linalg.lstsq(A, image_array2[:-1, -1], rcond=None)[0]
+
+## Multiply the last row of the original matrix by the coefficients and print the result
+#print(np.dot(coeff, image_array2[-1, :-1]))
 
